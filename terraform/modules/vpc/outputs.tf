96a56ef3c05df9d7c105f5fc3f93e273e98a6b29
rakidummy-account <rakeshgupta.chitluri@gmail.com>
2026-05-04 17:23:21 +0530

    [INFRA-9999] v0: terraform/modules/vpc/outputs.tf

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,75 @@
+################################## VPC ##################################
+
+output "vpc" {
+  description = "All attributes of the VPC."
+  value       = aws_vpc.this
+}
+
+################################## Internet Gateway ##################################
+
+output "internet_gateway" {
+  description = "All attributes of the Internet Gateway."
+  value       = aws_internet_gateway.this
+}
+
+################################## Subnets ##################################
+
+output "public_subnets" {
+  description = "All attributes of public subnets (map keyed by subnet logical name)."
+  value       = aws_subnet.public
+}
+
+output "private_subnets" {
+  description = "All attributes of private subnets (map keyed by subnet logical name)."
+  value       = aws_subnet.private
+}
+
+################################## Route Tables ##################################
+
+output "public_route_table" {
+  description = "All attributes of the public route table."
+  value       = aws_route_table.public
+}
+
+output "private_route_tables" {
+  description = "All attributes of private route tables (map keyed by route table logical name)."
+  value       = aws_route_table.private
+}
+
+################################## Elastic IPs ##################################
+
+output "nat_eips" {
+  description = "All attributes of NAT Gateway Elastic IPs (map keyed by NAT gateway logical name)."
+  value       = aws_eip.nat
+}
+
+################################## NAT Gateways ##################################
+
+output "nat_gateways" {
+  description = "All attributes of NAT Gateways (map keyed by NAT gateway logical name)."
+  value       = aws_nat_gateway.this
+}
+
+################################## Security Groups ##################################
+
+output "security_groups" {
+  description = "All attributes of security groups (map keyed by security group logical name)."
+  value       = aws_security_group.this
+}
+
+################################## Flow Logs ##################################
+
+output "cloudwatch_log_group" {
+  description = "All attributes of the CloudWatch Log Group for VPC Flow Logs."
+  value       = aws_cloudwatch_log_group.flow_logs
+}
+
+output "flow_log_iam_role" {
+  description = "All attributes of the IAM Role for VPC Flow Logs."
+  value       = aws_iam_role.flow_logs
+}
+
+output "flow_log" {
+  description = "All attributes of the VPC Flow Log."
+  value       = aws_flow_log.this
+}
