f1874f4cdabddc9ae7f6a4969c0e6782a36037e4
rakidummy-account <rakeshgupta.chitluri@gmail.com>
2026-05-04 20:06:41 +0530

    [INFRA-1234] v1: terraform/modules/s3-datalake/versions.tf

--- terraform/modules/s3-datalake/versions.tf
+++ terraform/modules/s3-datalake/versions.tf
@@ -3,9 +3,9 @@ terraform {
 
   required_providers {
     aws = {
-      source                = "localterraform.com/SSC/aws"
+      source                = "hashicorp/aws"
       version               = ">=5.0.0, <6.0.0"
-      configuration_aliases = [aws.target_region]
+      configuration_aliases = [aws.primary]
     }
   }
 }
