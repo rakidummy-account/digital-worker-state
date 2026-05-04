bd4af616b4893033b5a395caf3652fd839744cfe
rakidummy-account <rakeshgupta.chitluri@gmail.com>
2026-05-04 17:23:36 +0530

    [INFRA-9999] v1: terraform/modules/vpc/data.tf

--- terraform/modules/vpc/data.tf
+++ terraform/modules/vpc/data.tf
@@ -1,15 +1,15 @@
 data "aws_ssm_parameter" "core_tags" {
-  provider = aws.target_region
+  provider = aws.primary
   name     = "/aft/account-request/custom-fields/core_tags"
 }
 
 data "aws_kms_alias" "account_kms_key" {
-  provider = aws.target_region
+  provider = aws.primary
   name     = "alias/aft/account_kms_cmk"
 }
 
 locals {
-  core_tags   = jsondecode(data.aws_ssm_parameter.core_tags.insecure_value)
-  tags        = merge(local.core_tags, var.tags)
-  kms_key_arn = var.kms_key_arn == null ? data.aws_kms_alias.account_kms_key.target_key_arn : var.kms_key_arn
+  core_tags   = jsondecode(data.aws_ssm_parameter.core_tags.value)
+  merged_tags = merge(local.core_tags, var.tags)
+  kms_key_arn = var.kms_key_arn == null ? data.aws_kms_alias.account_kms_key.arn : var.kms_key_arn
 }
