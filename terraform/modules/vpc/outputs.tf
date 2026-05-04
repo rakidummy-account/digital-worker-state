572fe7e4e1fe4b9227293661bf7a5cd4e32dfee1
rakidummy-account <rakeshgupta.chitluri@gmail.com>
2026-05-04 17:23:56 +0530

    [INFRA-9999] v1: terraform/modules/vpc/outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -2,74 +2,74 @@
 
 output "vpc" {
   description = "All attributes of the VPC."
-  value       = aws_vpc.this
+  value       = aws_vpc.this.id
 }
 
 ################################## Internet Gateway ##################################
 
 output "internet_gateway" {
   description = "All attributes of the Internet Gateway."
-  value       = aws_internet_gateway.this
+  value       = aws_internet_gateway.this.id
 }
 
 ################################## Subnets ##################################
 
 output "public_subnets" {
   description = "All attributes of public subnets (map keyed by subnet logical name)."
-  value       = aws_subnet.public
+  value       = { for k, v in aws_subnet.public : k => v.id }
 }
 
 output "private_subnets" {
   description = "All attributes of private subnets (map keyed by subnet logical name)."
-  value       = aws_subnet.private
+  value       = { for k, v in aws_subnet.private : k => v.id }
 }
 
 ################################## Route Tables ##################################
 
 output "public_route_table" {
   description = "All attributes of the public route table."
-  value       = aws_route_table.public
+  value       = aws_route_table.public.id
 }
 
 output "private_route_tables" {
   description = "All attributes of private route tables (map keyed by route table logical name)."
-  value       = aws_route_table.private
+  value       = { for k, v in aws_route_table.private : k => v.id }
 }
 
 ################################## Elastic IPs ##################################
 
 output "nat_eips" {
   description = "All attributes of NAT Gateway Elastic IPs (map keyed by NAT gateway logical name)."
-  value       = aws_eip.nat
+  value       = { for k, v in aws_eip.nat : k => v.id }
 }
 
 ################################## NAT Gateways ##################################
 
 output "nat_gateways" {
   description = "All attributes of NAT Gateways (map keyed by NAT gateway logical name)."
-  value       = aws_nat_gateway.this
+  value       = { for k, v in aws_nat_gateway.this : k => v.id }
 }
 
 ################################## Security Groups ##################################
 
 output "security_groups" {
   description = "All attributes of security groups (map keyed by security group logical name)."
-  value       = aws_security_group.this
+  value       = { for k, v in aws_security_group.this : k => v.id }
 }
 
 ################################## Flow Logs ##################################
 
 output "cloudwatch_log_group" {
   description = "All attributes of the CloudWatch Log Group for VPC Flow Logs."
-  value       = aws_cloudwatch_log_group.flow_logs
+  value       = try(aws_cloudwatch_log_group.flow_logs[0].id, null)
 }
 
 output "flow_log_iam_role" {
   description = "All attributes of the IAM Role for VPC Flow Logs."
-  value       = aws_iam_role.flow_logs
+  value       = try(aws_iam_role.flow_logs[0].id, null)
 }
 
 output "flow_log" {
   description = "All attributes of the VPC Flow Log."
-  value       = aws_flow_log.this
+  value       = try(aws_flow_log.this[0].id, null)
 }
